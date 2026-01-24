efced81aa9e522eff010ffb7bd2678ffbb0b3612
Josef Bálek <96839052+VesnickyTrombonista@users.noreply.github.com>
2026-01-24 09:40:44 +0100

    Add opengraph tags (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://studentskyvelehrad.cz',
   vite: {
     plugins: [tailwindcss()]
   }
