611b392a2611c5a54725735e17a4888f1e7540a5
Josef Bálek <Pepik.Balek@seznam.cz>
2026-01-24 12:33:36 +0100

    Edit menu and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,17 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://studentskyvelehrad.cz',
+  server: {
+    port: 4321,
+    host: true
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
   },
   integrations: [],
 });
