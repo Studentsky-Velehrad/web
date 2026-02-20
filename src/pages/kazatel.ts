0fd84c37363faf2a2aa314b94f5fcd92301abcb6
„Ondrej“ <kolekondrej2@seznam.cz>
2026-02-20 12:27:39 +0100

    kazatel

---
+++ src/pages/kazatel.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const url = new URL("/kazatel.pdf", request.url);
+  const response = await fetch(url);
+
+  return new Response(response.body, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "inline",
+    },
+  });
+};
